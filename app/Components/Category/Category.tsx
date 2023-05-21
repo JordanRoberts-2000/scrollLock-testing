437efc190eab438ee6a47b64307fef4d597218e3
Jordan <jordanroberts333@icloud.com>
2023-05-21 22:00:59 +0100

    fixed fixed positioning

--- app/Components/Category/Category.tsx
+++ app/Components/Category/Category.tsx
@@ -35,7 +35,6 @@ const Category = ({children, imageUrl, title, subtitle, priceOptions, galleryIma
     let imageWrapperRef = useRef<HTMLDivElement>(null)
     let categoryScrollRef = useRef<HTMLDivElement>(null)
     let infoSectionWrapper = useRef<HTMLDivElement>(null)
-    let aspectWrapper = useRef<HTMLDivElement>(null)
     let transitioning = useRef(false)
     const { categoryClicked } = useStore()
     const [active, setActive] = useState(false)
@@ -58,7 +57,7 @@ const Category = ({children, imageUrl, title, subtitle, priceOptions, galleryIma
             requestAnimationFrame(() => {
                 imageRef.current.style.transitionDuration = `600ms`
                 imageRef.current.style.transform = `translate(0, 0) scale(1)`
-                aspectWrapper.current!.style.aspectRatio = '3/3.3'
+                // aspectWrapper.current!.style.aspectRatio = '3/3.3'
             })
             let timer = 0
             const checkTop = setInterval(() => {
@@ -88,7 +87,7 @@ const Category = ({children, imageUrl, title, subtitle, priceOptions, galleryIma
                     clearInterval(checkTop)
                     setPriceActive(true)
                     transitioning.current = false
-                    aspectWrapper.current!.style.aspectRatio = '3/2'
+                    // aspectWrapper.current!.style.aspectRatio = '3/2'
                     document.documentElement.style.paddingRight = `0px`
                     document.documentElement.style.overflow = 'auto'
                     document.documentElement.style.touchAction = 'auto'
@@ -114,7 +113,7 @@ const Category = ({children, imageUrl, title, subtitle, priceOptions, galleryIma
                     clearInterval(checkTop)
                     setImageFixed(false)
                     requestAnimationFrame(() => {
-                        aspectWrapper.current!.style.aspectRatio = '3/2'
+                        // aspectWrapper.current!.style.aspectRatio = '3/2'
                         infoSectionWrapper.current!.style.gridTemplateRows = '0fr'
                         infoSectionWrapper.current!.style.transitionDuration = `300ms`
                         imageRef.current.style.transitionDuration = `550ms`
@@ -140,7 +139,7 @@ const Category = ({children, imageUrl, title, subtitle, priceOptions, galleryIma
                 }
                 if(timer >= 600){
                     // Stage 3 - cancel
-                    aspectWrapper.current!.style.aspectRatio = '3/3.3'
+                    // aspectWrapper.current!.style.aspectRatio = '3/3.3'
                     useStore.setState(() => ({ footerExtended: true }))
                     clearInterval(checkTop)
                     transitioning.current = false
@@ -157,7 +156,7 @@ const Category = ({children, imageUrl, title, subtitle, priceOptions, galleryIma
                     ${categoryClicked !== title && categoryClicked !== '' ? 'opacity-50 duration-200' : 'opacity-100 duration-500'} 
                     overscroll-contain select-none grid-cols-[2fr,1fr] auto-rows-min mx-auto`}>
                 {/* Image */}
-                <div ref={aspectWrapper} className={`${active ? "pointer-events-auto" : 'pointer-events-none'} w-[100%] aspect-[3/2] duration-500 mx-auto relative`} onClick={() => handleExit()}>
+                <div className={`${active ? "pointer-events-auto" : 'pointer-events-none'} w-[100%] aspect-[3/3.3] duration-500 mx-auto relative`} onClick={() => handleExit()}>
                 <CategoryImage imageUrl={imageUrl} active={active} title={title} subtitle={subtitle} index={index} blurImageUrl={blurImageUrl} imageRef={imageRef} 
                                 imageWrapperRef={imageWrapperRef} imageFixed={imageFixed} transitioning={transitioning} scrollUpRef={scrollUpRef}/>
                 </div>
--- app/Components/Category/CategoryImage.tsx
+++ app/Components/Category/CategoryImage.tsx
@@ -56,7 +56,7 @@ const CategoryImage = ({imageUrl, active, title, subtitle, blurImageUrl, index, 
     },[])
     return (
         <div ref={imageWrapperRef} className="h-full w-full relative duration-700 overflow-hidden">
-                <div className={`${imageFixed && "fixed top-0 left-0 w-full aspect-[3/3.3] z-30 overflow-hidden "} relative h-full w-full`}>
+                <div className={`${imageFixed ? "fixed top-0 left-0 w-full aspect-[3/3.3] z-30 overflow-hidden" : 'relative h-full w-full'}`}>
                 <div className="absolute top-0 w-full h-full bg-white/40 z-10"></div>
                 <Image ref={imageRef} alt="placeholder" priority={index <= 1} fill src={imageUrl} className={`${powerSavingMode && '!scale-100'} object-cover select-none scale-150 duration-75`} 
                         placeholder="blur" blurDataURL={blurImageUrl} sizes="100vw"/>
@@ -69,4 +69,4 @@ const CategoryImage = ({imageUrl, active, title, subtitle, blurImageUrl, index, 
     )
 }
 
-export default memo(CategoryImage)
+export default CategoryImage
