a460842da264e12180282cbf8df3ad14ff6c166e
Jordan <jordanroberts333@icloud.com>
2023-05-22 10:07:52 +0100

    fixed aspect

--- app/Components/Category/CategoryImage.tsx
+++ app/Components/Category/CategoryImage.tsx
@@ -28,7 +28,7 @@ const CategoryImage = ({imageUrl, active, title, subtitle, blurImageUrl, index, 
         throttle.current = false
         setTimeout(() => {
             throttle.current = true
-        }, 10)
+        }, 20)
         requestAnimationFrame(() => {
             if(imageWrapperRef.current!.getBoundingClientRect().top <= window.innerHeight && imageWrapperRef.current!.getBoundingClientRect().top >= -imageWrapperRef.current!.getBoundingClientRect().height){
                 let percentagePassed = ((imageWrapperRef.current!.getBoundingClientRect().top - window.innerHeight)*-1)/(window.innerHeight + imageWrapperRef.current!.getBoundingClientRect().height)
@@ -56,7 +56,7 @@ const CategoryImage = ({imageUrl, active, title, subtitle, blurImageUrl, index, 
     },[])
     return (
         <div ref={imageWrapperRef} className="h-full w-full relative duration-700 overflow-hidden">
-                <div className={`${imageFixed ? "fixed top-0 left-0 w-full aspect-[3/3.1] z-30 overflow-hidden" : 'relative h-full w-full'}`}>
+                <div className={`${imageFixed ? "fixed top-0 left-0 w-full aspect-[3/3.3] z-30 overflow-hidden" : 'relative h-full w-full'}`}>
                 <div className="absolute top-0 w-full h-full bg-white/40 z-10"></div>
                 <Image ref={imageRef} alt="placeholder" priority={index <= 1} fill src={imageUrl} className={`${powerSavingMode && '!scale-100'} object-cover transition-transform select-none scale-150 duration-75`} 
                         placeholder="blur" blurDataURL={blurImageUrl} sizes="100vw"/>
